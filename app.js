7ebc41ebc8e6e8b8fee76111f75c0ecd2e867bfd
GigaChad <84654653+Mayank-mic@users.noreply.github.com>
2024-03-07 14:16:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,29 +15,53 @@ form.addEventListener('submit', (e) => {
     let values;
     values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
-    if (type.value === 'invoice') {
-        doc = new Invoice(...values);
-    }
-    else {
-        doc = new Payment(...values);
-    }
-    list.render(doc, type.value, 'end');
-    form.reset();
 
-    const successMessage = document.getElementById('successMessage');
+    if(values[0]==="" || values[1]==="" || !values[2]){
+
+    const failureMessage = document.getElementById('failureMessage');
   
-    function showSuccessMessage() {
-      successMessage.style.display = 'flex';
+    function showFailureMessage() {
+      failureMessage.style.display = 'flex';
       setTimeout(function() {
-        successMessage.style.display = 'none';
+        failureMessage.style.display = 'none';
       }, 1000);
     }
     
     // Simulate adding to the list
     // Call showSuccessMessage() when item is successfully added
     setTimeout(() => {
-      showSuccessMessage();
+      showFailureMessage();
     }, 1000);
+
+        
+    }else{
+
+    
+            if (type.value === 'invoice') {
+                doc = new Invoice(...values);
+            }
+            else {
+                doc = new Payment(...values);
+            }
+            list.render(doc, type.value, 'end');
+            form.reset();
+        
+            const successMessage = document.getElementById('successMessage');
+          
+            function showSuccessMessage() {
+              successMessage.style.display = 'flex';
+              setTimeout(function() {
+                successMessage.style.display = 'none';
+              }, 1000);
+            }
+            
+            // Simulate adding to the list
+            // Call showSuccessMessage() when item is successfully added
+            setTimeout(() => {
+              showSuccessMessage();
+            }, 1000);
+
+    }
 
 });
 
